b4fed6f86ebe514745f9e3015493a37188e27545
Manuel Silva <manuel.silvag1@gmail.com>
2020-05-14 06:22:47 -0500

    fix module 7

--- src/index.js
+++ src/index.js
@@ -59,18 +59,24 @@ async function getBooksOrMoviesAsync() {
     return values;
   } catch (error) {
     console.error("Error waiting for the promise race", error);
+    throw error;
   }
 }
 
 getBooksAndMoviesAsync().then((results) => {
-  console.log("movies and books", {
+  console.log("movies AND books", {
     movies: results.movies,
     books: results.books,
   });
+})
+.catch((error) => {
+  console.error("Error in getBooksAndMoviesAsync execution", error);
 });
 
-getBooksOrMoviesAsync().then((results) => {
+getBooksOrMoviesAsync().then(results => {
   console.log("movies OR books", {
     results,
   });
-});
+}).catch(error =>
+  console.error("Error in getBooksOrMoviesAsync execution", error)
+);
