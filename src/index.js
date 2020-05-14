25f39f21af5e33176913d1f4358e02993c042a98
Manuel Silva <manuel.silvag1@gmail.com>
2020-05-14 06:13:54 -0500

    module 7 completed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-import { fetchTimeOut, fetchMovies, fetchBooks } from "./services";
+import {
+  fetchTimeOut,
+  fetchMovies,
+  fetchBooks,
+  asyncFetchBooks,
+  asyncFetchMovies,
+} from "./services";
 const movies = require("./data/movies.json");
 
 function getBooksAndMovies() {
@@ -30,4 +36,41 @@ const getBooksOrMoviesPromise = getBooksOrMovies();
 
 getBooksOrMoviesPromise.then((results) => {
   console.log("getBooksOrMoviesPromise", results);
+});
+
+async function getBooksAndMoviesAsync() {
+  try {
+    const [books, movies] = await Promise.all([
+      asyncFetchBooks(),
+      asyncFetchMovies(),
+    ]);
+    return {
+      books,
+      movies,
+    };
+  } catch (error) {
+    console.log("Error fetching books and movies", error);
+  }
+}
+
+async function getBooksOrMoviesAsync() {
+  try {
+    const values = await Promise.race([asyncFetchBooks(), asyncFetchMovies()]);
+    return values;
+  } catch (error) {
+    console.error("Error waiting for the promise race", error);
+  }
+}
+
+getBooksAndMoviesAsync().then((results) => {
+  console.log("movies and books", {
+    movies: results.movies,
+    books: results.books,
+  });
+});
+
+getBooksOrMoviesAsync().then((results) => {
+  console.log("movies OR books", {
+    results,
+  });
 });
